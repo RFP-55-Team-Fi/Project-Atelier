d4ee6e55c22cdc19ad4b89396f20569349e9515d
Aaron Tran <altran32@gmail.com>
2021-10-02 01:36:41 -0700

    add queries for add review.

--- server/controller/queries.js
+++ server/controller/queries.js
@@ -1,4 +1,5 @@
 const { query } = require('../../db/index.js')
+
 module.exports = {
   getReviews: (req, res) => {
     let { product_id, count, page, sort } = req.body;
@@ -19,23 +20,22 @@ module.exports = {
   },
   addReview: (req, res) => {
     const date = new Date().getTime().toString();
-    const {
-      product_id, rating, summary, body,
-      recommend, reviewer_name, reviewer_email, photos, characteristics, response
-     } = req.body;
-     console.log('product_id:', product_id,'rating:', rating, 'summary',summary, 'body',body,
-      recommend,':recommend', reviewer_name,':name', reviewer_email,':email', photos,':photos','characteristics:', characteristics, 'response:', response, 'date', date)
-    // const text = `
-    // insert into reviews (product_id, rating, date, summary, body, recommend, reviewer_name, reviewer_email, response)
-    // values ($1, $2, $3, $4, $5, $6, $7, $8, $9)`
-    const text =
-    `INSERT INTO reviews(product_id, rating, date, summary, body, recommend, reviewer_name, reviewer_email, reported, helpfulness) VALUES (${product_id}, ${rating}, '${date}', '${summary}', '${body}', ${recommend}, '${reviewer_name}', '${reviewer_email}', false, 0)`;
-    // query(text, [product_id, rating, date, summary, body, recommend, reviewer_name, reviewer_email, response ])
-    query(text)
+    const { product_id, rating, summary, body, recommend, reviewer_name, reviewer_email, photos, characteristics, response } = req.body;
+    const text = `insert into reviews (product_id, rating, date, summary, body, recommend, reviewer_name, reviewer_email, response) values ($1, $2, $3, $4, $5, $6, $7, $8, $9) returning review_id`;
+    const text2 = `insert into characteristic_reviews (characteristic_id, review_id, value) values ($11, $11, $12)`;
+    const text3 = `insert into reviews_photos (review_id, url) values ($1, $2);`;
+    const text4 = `insert into characteristics (product_id, name) values ($15, $16)`;
+    const isPhotos = photos.length > 0 ? true : false;
+    console.log(isPhotos)
+    query(text, [product_id, rating, date, summary, body, recommend, reviewer_name, reviewer_email, response ])
+    .then(result => {
+      const { review_id } = result.rows[0];
+      query(text3, [review_id, photos])
+        .then(result=> console.log(result, 'result from text 3'))
+        .catch(err => console.log(err, 'err from text 3'))
+    })
     .then(result => res.status(201).send(result))
     .catch(err => res.send(err))
-
-
   },
   markHelpfulReview: (req, res)=>{
     const { review_id } = req.params;
@@ -57,76 +57,76 @@ module.exports = {
 
 }
 
-const view = {
-  "product": "2",
-  "page": 0,
-  "count": 5,
-  "results": [
-    {
-      "review_id": 841126,
-      "rating": 5,
-      "summary": "Very good",
-      "recommend": true,
-      "response": null,
-      "body": "lorem ipsum",
-      "date": "2021-09-17T00:00:00.000Z",
-      "reviewer_name": "tester",
-      "helpfulness": 37,
-      "photos": [
-        {
-            "id": 1235676,
-            "url": "https://images.unsplash.com/photo-1465877783223-4eba513e27c6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"
-        },
-        {
-            "id": 1235677,
-            "url": "https://images.unsplash.com/photo-1555274175-6cbf6f3b137b?ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80"
-        }
-      ]
-    },
-    {
-      "review_id": 841149,
-      "rating": 5,
-      "summary": "Very good",
-      "recommend": true,
-      "response": null,
-      "body": "lorem ipsum",
-      "date": "2021-09-17T00:00:00.000Z",
-      "reviewer_name": "tester",
-      "helpfulness": 1,
-      "photos": []
-    },
-  ]
-}
+// const view = {
+//   "product": "2",
+//   "page": 0,
+//   "count": 5,
+//   "results": [
+//     {
+//       "review_id": 841126,
+//       "rating": 5,
+//       "summary": "Very good",
+//       "recommend": true,
+//       "response": null,
+//       "body": "lorem ipsum",
+//       "date": "2021-09-17T00:00:00.000Z",
+//       "reviewer_name": "tester",
+//       "helpfulness": 37,
+//       "photos": [
+//         {
+//             "id": 1235676,
+//             "url": "https://images.unsplash.com/photo-1465877783223-4eba513e27c6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"
+//         },
+//         {
+//             "id": 1235677,
+//             "url": "https://images.unsplash.com/photo-1555274175-6cbf6f3b137b?ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80"
+//         }
+//       ]
+//     },
+//     {
+//       "review_id": 841149,
+//       "rating": 5,
+//       "summary": "Very good",
+//       "recommend": true,
+//       "response": null,
+//       "body": "lorem ipsum",
+//       "date": "2021-09-17T00:00:00.000Z",
+//       "reviewer_name": "tester",
+//       "helpfulness": 1,
+//       "photos": []
+//     },
+//   ]
+// }
 
-const meta = {
-  "product_id": "40344",
-  "ratings": {
-      "1": "7",
-      "2": "11",
-      "3": "12",
-      "4": "21",
-      "5": "92"
-  },
-  "recommended": {
-      "false": "32",
-      "true": "111"
-  },
-  "characteristics": {
-      "Fit": {
-          "id": 135219,
-          "value": "2.5000000000000000"
-      },
-      "Length": {
-          "id": 135220,
-          "value": "2.5757575757575758"
-      },
-      "Comfort": {
-          "id": 135221,
-          "value": "2.6562500000000000"
-      },
-      "Quality": {
-          "id": 135222,
-          "value": "2.9166666666666667"
-      }
-  }
-}
+// const meta = {
+//   "product_id": "40344",
+//   "ratings": {
+//       "1": "7",
+//       "2": "11",
+//       "3": "12",
+//       "4": "21",
+//       "5": "92"
+//   },
+//   "recommended": {
+//       "false": "32",
+//       "true": "111"
+//   },
+//   "characteristics": {
+//       "Fit": {
+//           "id": 135219,
+//           "value": "2.5000000000000000"
+//       },
+//       "Length": {
+//           "id": 135220,
+//           "value": "2.5757575757575758"
+//       },
+//       "Comfort": {
+//           "id": 135221,
+//           "value": "2.6562500000000000"
+//       },
+//       "Quality": {
+//           "id": 135222,
+//           "value": "2.9166666666666667"
+//       }
+//   }
+// }
