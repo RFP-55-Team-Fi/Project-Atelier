c193fa65276c1b22627a28653854ad0ba4195641
Aaron Tran <altran32@gmail.com>
2021-10-03 16:35:19 -0700

    update k6 options

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
-import http from 'k6/http';
-import { sleep, check } from 'k6';
-const { server_port } = require('./db/config.js');
+import http from "k6/http";
+import { sleep, check } from "k6";
 
+const { server_port } = require("./db/config.js");
+
+// export const options = {
+//   vus: 10,
+//   duration: "10s",
+// };
+
+export const options = {
+  stages: [
+    { duration: "5s", target: 20 },
+    { duration: "5s", target: 200 },
+    { duration: "5s", target: 700 },
+    { duration: "30s", target: 2700 },
+    { duration: "10s", target: 300 },
+    { duration: "5s", target: 10 },
+  ],
+};
 export default function () {
-  // let options = {
-  //   vus:10, duration: "10s",
-  // }
-
-  let res = http.get(`http://localhost:${server_port}/reviews/`);
-  check(res, { 'status was 200': (r) => r.status == 200 });
+  const res = http.get(`http://localhost:${server_port}/reviews/`);
+  check(res, { "status was 200": (r) => r.status === 200 });
   sleep(1);
 }
